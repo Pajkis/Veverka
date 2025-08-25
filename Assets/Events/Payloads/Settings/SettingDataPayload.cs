be9168faa993fc5fe399674172160cc2afb2b1bc
VojirM <martin.vojir@gmail.com>
2025-08-25 19:10:48 +0900

    Adds setting data events and payload
    Implements events for broadcasting and requesting setting data.
    
    Introduces a payload struct for carrying setting data.
    
    These changes facilitate a more robust and decoupled system for managing game settings.

---
+++ Assets/Events/Payloads/Settings/SettingDataPayload.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// Payload for broadcasting game setting values.
+/// </summary>
+public struct SettingDataPayload
+{
+    public GameSettingsEnum Setting;
+    public float Value;
+}
