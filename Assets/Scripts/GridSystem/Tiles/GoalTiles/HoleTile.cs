12add0f8f4e1112b78a51578be18441dd8a0bad7
VojirM <martin.vojir@gmail.com>
2025-08-29 16:55:56 +0900

    Enhance holetile goal reaction

--- Assets/Scripts/GridSystem/Tiles/GoalTiles/HoleTile.cs
+++ Assets/Scripts/GridSystem/Tiles/GoalTiles/HoleTile.cs
@@ -13,15 +13,27 @@ public class HoleTile : GoalTile
         // Check if the pushable is in the goal position
         if (GridPosition == payload.Position)
         {
+
             // Play goal reached sound effect
             playSfxEvent.Raise(SfxType.GoalReached);
             // Raise goal reached event
 
-            goalRemovedEvent.Raise(new GoalBasicPayload
+            if (payload.NutType == NutType.StoneNut)
             {
-                Position = payload.Position,
-            });
-            Destroy(gameObject);
+                goalRemovedEvent.Raise(new GoalBasicPayload
+                {
+                    Position = payload.Position,
+                    GoalReduction = 0,
+                });
+                Destroy(gameObject);
+
+                // Set wall in the position of the hole
+                roadSetEvent.Raise(new RoadBasicPayload
+                {
+                    Position = payload.Position,
+                    RoadType = RoadType.StoneRoad,
+                });
+            }                    
         }
     }
     #endregion
